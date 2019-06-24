18f992cc366be470050ef1705c39954f2326db44
EC2 Default User <ec2-user@ip-172-31-45-226.eu-central-1.compute.internal>
2019-06-24 16:39:48 +0000

    List View for Admin Page

--- backend/admin.py
+++ backend/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from backend.models import react360, EventTracker
+from backend.models import react360, EventTracker, FullScreen
 
+class EventList(admin.ModelAdmin):
+    list_display= ('timestamp', 'item', 'event')
+    
+class ReactList(admin.ModelAdmin):
+    list_display= ('timestamp', 'x_axis', 'y_axis')
+    
+class FullScreenList(admin.ModelAdmin):
+    list_display= ('timestamp', 'page', 'event')
+    
 
 # Register your models here.
-admin.site.register(react360)
-admin.site.register(EventTracker)
+admin.site.register(react360, ReactList)
+admin.site.register(EventTracker, EventList)
+admin.site.register(FullScreen, FullScreenList)
+
